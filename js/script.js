8ef2c0a66f97a8824a1ed0b6efe7cfee0c9fcfd8
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-10-29 23:37:59 +0530

    set the responsivness for the minimum resolution

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 //animated header
 $(function() {
     $(window).scroll(function() {
--- ts/navigator.ts
+++ ts/navigator.ts
@@ -1,16 +1,25 @@
 import $ from 'jquery';
 
-$('.nav-about').on('click', ()=>{
+const secondHeaderHtml = `  <div class="secondary-header">
+                            <menu class="secondary-header-menu">
+                                <li class="menu-item nav-item">Home</li>
+                                <li class="menu-item nav-item">About</li>
+                                <li class="menu-item nav-item">Portfolio</li>
+                                <li class="menu-item nav-item">Projects</li>
+                                <li class="menu-item nav-item">Contact</li>
+                            </menu>
+                            </div>`;
 
-    console.log("done");
-    // console.log("done2");
-    
-    
-    $('html, body').animate({
-        scroll: $('portfolio-container__3dmod').offset()?.top
-    }, 1000);
-});
 
-$('.hamburger__pos').on('click', ()=>{
+
+$('.menu-hamburger').on('click', ()=>{
     
+    if($('.secondary-header').length === 0){
+        $('.header').append(secondHeaderHtml);
+        $('.header').addClass('bg-tr');
+    }else{
+        $('.secondary-header').remove();
+        $('.header').removeClass('bg-tr');
+    }
+
 });
