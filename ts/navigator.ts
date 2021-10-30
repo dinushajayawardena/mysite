ff0cc7c3edaf5dc7c4f95ec647e75dc4d269642d
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-10-30 13:08:18 +0530

    implement hamburger visibility according to viewport width

--- ts/navigator.ts
+++ ts/navigator.ts
@@ -10,9 +10,28 @@ const secondHeaderHtml = `  <div class="secondary-header">
                             </menu>
                             </div>`;
 
+const humburgerHtml = ` <div class="menu-hamburger hamburger__pos">
+                            <i class="fas fa-bars"></i>
+                        </div>`;
 
 
-$('.menu-hamburger').on('click', ()=>{
+var docWidth: number;
+$(window).resize(function(){
+    docWidth = $( window ).width();
+    console.log(docWidth);
+    
+    if(docWidth >= 992){
+        if($('.menu-hamburger').length !== 0){
+            $('.menu-hamburger').remove();
+            $('.secondary-header').remove();
+        } 
+        
+    }else{
+        if($('.menu-hamburger').length === 0) $('.hamburger-wrapper').append(humburgerHtml);
+    }
+});
+
+$('.hamburger-wrapper').on('click', ()=>{
     
     if($('.secondary-header').length === 0){
         $('.header').append(secondHeaderHtml);
